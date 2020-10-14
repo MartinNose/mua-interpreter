f7471db66b101e9125d9213b422259105a79806b
MartinNose <liucmartin@qq.com>
2020-10-15 00:51:30 +0800

    basic framework done

--- src/mua/Interpreter.java
+++ src/mua/Interpreter.java
@@ -1,11 +1,12 @@
 package mua;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Interpreter {
-    private String[] BasicOperations = {"make", "thing", "print", "read",
-                                        "add", "sub", "mul", "div", "mod",
-                                        };
-
     public static void mainLoop() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -31,16 +32,23 @@ public class Interpreter {
     private static int readLine(String line) {
         String[] literals = line.split("\\s+");
         State state = State.NORMAL;
+        ArrayList<String> operationStack = new ArrayList<String>();
+        ArrayList<String> valueStack = new ArrayList<String>();
+        Map<String, String> variables = new HashMap<String, String>()
+        ArrayList<String> basicOperations = new ArrayList<>(Arrays.asList(Operations.BasicOperations));
+
         for (String literal : literals) {
             System.out.println(literal);
 
             switch (state) {
                 case NORMAL :
-                    switch(literal.charAt(0)) {
-                        case '"': 
-                            // Handle word
-                            break;
-                        
+                    if (literal.charAt(0) == ':') {
+                        //TODO eval the value and put to Stack
+                    } 
+                    if (basicOperations.contains(literal)) {
+                        operationStack.add(literal);
+                    } else {
+                        valueStack.add(literal);
                     }
                     break;
                 case LISBEG :
@@ -49,6 +57,13 @@ public class Interpreter {
                     break;
             }
         }
+
+// TODO pop the operations and computes
+
+
+
+
+
         return 0;
     }
 }
